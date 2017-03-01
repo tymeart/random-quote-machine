bac39945f92b9c55f25711069372bb48dd28a7c1
Tiffany Lam <tlam5@sbcglobal.net>
2017-03-01 14:05:04 -0800

    Refactor Twitter div into actual button

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 	function getQuote(){
 		$.getJSON('https://crossorigin.me/http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en', function(data){
 				$('.quote').html(data.quoteText);
-			$('a.twitter').attr('href', 'https://twitter.com/home?status="' + data.quoteText + '"' + ' -' + data.quoteAuthor);
+			$('.twitter a').attr('href', 'https://twitter.com/home?status="' + data.quoteText + '"' + ' -' + data.quoteAuthor);
 				$('.quote-source').html('<p>' + data.quoteAuthor + '</p>');
 			})
 	};
